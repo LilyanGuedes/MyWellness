0a1f33239d574d0cd2866052f0b8e3ade6b43f69
LilyanGuedes <108057303+LilyanGuedes@users.noreply.github.com>
2023-05-20 22:06:36 -0300

    feat components

---
+++ src/pages/training.js
@@ -0,0 +1,213 @@
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View, Image, FlatList, TouchableOpacity, Linking } from 'react-native';
+
+const urlCardio = 'https://blog.hoppernutrition.com.br/treino-cardio/';
+const urlMuscula = 'https://www.tuasaude.com/beneficios-da-musculacao/';
+const urlPosterior = 'https://www.youtube.com/watch?v=LZmu7S__dU0&t=63s';
+const urlPernas = 'https://www.youtube.com/watch?v=ro42Rovk0GA&t=108s';
+const urlCostas = 'https://www.youtube.com/watch?v=syGd1VJkoMA&t=15s';
+const urlBiceps = 'https://www.youtube.com/watch?v=RoRALAYTSLs';
+const urlOmbro = 'https://www.youtube.com/watch?v=jhOiqGuECmw';
+const urlPeito = 'https://www.youtube.com/watch?v=b_WvXDvPkd8';
+
+function abrirURLCardio() {
+  Linking.openURL(urlCardio);
+}
+
+function abrirURLMuscula() {
+  Linking.openURL(urlMuscula);
+}
+
+function abrirURLPosterior() {
+  Linking.openURL(urlPosterior);
+}
+
+function abrirURLPernas() {
+  Linking.openURL(urlPernas);
+}
+
+function abrirURLCostas() {
+  Linking.openURL(urlCostas);
+}
+
+function abrirURLBiceps() {
+  Linking.openURL(urlBiceps);
+}
+
+function abrirURLOmbro() {
+  Linking.openURL(urlOmbro);
+}
+
+function abrirURLPeito() {
+  Linking.openURL(urlPeito);
+}
+
+
+export default function App() {
+
+  const DataCardio = [
+    { id: 1, title: '- Melhora a resistência' },
+    { id: 2, title: '- Melhora a eficiência da circulação sanguínea' },
+    { id: 3, title: '- Aumento da capacidade respiratória' },
+    { id: 4, title: '- Fortalece o coração' }
+  ]
+
+  const DataMuscula = [
+    { id: 1, title: '- Melhora da densidade óssea' },
+    { id: 2, title: '- Melhora da postura e alinhamento corporal' },
+    { id: 3, title: '- Aumento da força muscular' },
+    { id: 4, title: '- Redução do risco de lesões' }
+  ]
+  return (
+
+    <View style={styles.container}>
+
+      <View style={styles.head}>
+        <Image source={require('./assets/Logo2.png')}
+          style={{ width: 210, height: 320 }}
+          resizeMode="contain" />
+      </View>
+      <View style={styles.linha}></View>
+
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+
+        <View style={styles.conteudo}>
+          <Text style={styles.titulo}>A importância da atividade física</Text>
+          <Text>A atividade física é importante para a saúde cardiovascular, controle de peso, fortalecimento muscular e ósseo, melhora do humor, redução do estresse a e prevenção de doenças crônicas. É recomendado praticar pelo menos 150 minutos de atividade física moderada por semana para obter esses benefícios.
+          </Text>
+        </View>
+
+
+        <View style={styles.conteudo}>
+          <Text style={styles.titulo}>Treino cardiorrespiratório</Text>
+          <FlatList
+            numColumns={1}
+            keyExtractor={(item) => item.id.toString()}
+            data={DataCardio}
+            renderItem={({ item }) => (
+              <Text>{item.title}</Text>
+            )}
+          />
+          <TouchableOpacity>
+            <Text style={styles.mais} onPress={abrirURLCardio}>Saiba mais</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.conteudo}>
+          <Text style={styles.titulo}>Treino de Musculação</Text>
+          <FlatList
+            numColumns={1}
+            keyExtractor={(item) => item.id.toString()}
+            data={DataMuscula}
+            renderItem={({ item }) => (
+              <Text>{item.title}</Text>
+            )}
+          />
+          <TouchableOpacity>
+            <Text style={styles.mais} onPress={abrirURLMuscula}>Saiba mais</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.conteudo}>
+
+          <Text style={styles.titulo}>Treinos para iniciantes</Text>
+
+           <View style={styles.treinos}>
+              <TouchableOpacity>
+                <Text style={styles.videos} onPress={abrirURLPosterior}>1.Posterior</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.videos} onPress={abrirURLPernas}>2.Pernas</Text>
+              </TouchableOpacity>
+           </View>
+
+           <View style={styles.treinos}>
+              <TouchableOpacity>
+                <Text style={styles.videos} onPress={abrirURLCostas}>3.Costas</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.videos} onPress={abrirURLBiceps}>4.Bíceps</Text>
+              </TouchableOpacity>
+           </View>
+
+           <View style={styles.treinos}>
+              <TouchableOpacity>
+                <Text style={styles.videos} onPress={abrirURLOmbro}>5.Ombro</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.videos} onPress={abrirURLPeito}>6.Peito</Text>
+              </TouchableOpacity>
+           </View>
+          
+          
+        </View>
+
+        <StatusBar style="auto" />
+      </ScrollView>
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5B9053',
+    alignItems: 'center'
+
+  }, conteudo: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 45,
+    width: 362,
+    height: 210,
+    marginTop: 45,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5
+
+  }, logo: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  }, head: {
+    marginTop: 42,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 375,
+    height: 55
+  }, linha: {
+    backgroundColor: '#FFFFFF',
+    height: 0.34,
+    width: 408,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5
+
+  }, mais: {
+    color: '#66B25A',
+    fontWeight: 'bold',
+    width: 82,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }, titulo: {
+    fontWeight: 'bold',
+    marginBottom: 12
+  },treinos: {
+    flexDirection: 'row'
+  }, videos: {
+    color: '#5B9053',
+    fontWeight: 'bold',
+    padding: 12,
+    fontSize: 16
+  }
+
+});
---
+++ src/pages/water.js
@@ -0,0 +1,146 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, TextInput, Linking } from 'react-native';
+import React, { useState } from 'react';
+
+const urlSaibaMais = 'https://www.clinicaunavita.com.br/noticias/quantos-litros-de-agua-voce-precisa-beber-diariamente'
+
+function SaibaMais() {
+  Linking.openURL(urlSaibaMais);
+}
+
+
+export default function App() {
+
+  const [valor, setValor] = useState('');
+  const [resultado, setResultado] = useState(0);
+
+  const calcularResultado = () => {
+    const valorNumerico = parseFloat(valor);
+    if (!isNaN(valorNumerico)) {
+      const novoResultado = valorNumerico * 0.035;
+      setResultado(novoResultado.toFixed(2));
+    }
+  }
+
+  const beneficios = [
+    { id: 1, title: '- Regulação da temperatura corporal' },
+    { id: 2, title: '- Transporte de nutrientes' },
+    { id: 3, title: '- Eliminação de resíduos' },
+    { id: 4, title: '- Lubrificação das articulações' },
+    { id: 5, title: '- Manutenção do equilíbrio hídrico' }
+  ]
+
+  return (
+
+    <View style={styles.container}>
+
+      <View style={styles.head}>
+        <Image source={require('./assets/Logo2.png')}
+          style={{ width: 210, height: 320 }}
+          resizeMode="contain" />
+      </View>
+      <View style={styles.linha}></View>
+
+      <StatusBar style="auto" />
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
+        <Text style={{ fontSize: 18, marginTop: 20, color: '#FFFFFF' }}>Você sabe quanto de água deve ingerir?</Text>
+        <Image source={require('./assets/agua-diaria.png')}
+          style={{ width: 98, height: 208 }}
+          resizeMode="contain" />
+
+        <TextInput
+          style={{ width: 200, height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 20, paddingHorizontal: 10 }}
+          keyboardType="numeric"
+          placeholder="Digite o seu peso"
+          onChangeText={texto => setValor(texto)}
+          value={valor}
+        />
+        <TouchableOpacity onPress={calcularResultado} style={styles.btnCalcular}>
+          <Text>Calcular</Text>
+        </TouchableOpacity>
+        {resultado > 0 &&
+          <Text style={{ fontSize: 16, marginTop: 20, color: '#FFFFFF', height: 300 }}>
+            Ingerir aproximadamente: {resultado} litros
+            
+            <View style={styles.conteudoAtençao}>
+              <Image source={require('./assets/ponto-de-exclamacao.png')}
+                style={{ width: 30, height: 30 }}
+                resizeMode="contain" />
+              <Text>Praticantes de atividade física precisam tomar mais de cerca de 500 ml a 1 L de água por cada hora de atividade. As diferenças de temperatura também influenciam a recomendação.</Text>
+              <TouchableOpacity>
+                <Text style={styles.mais} onPress={SaibaMais}>Saiba mais</Text>
+              </TouchableOpacity>
+            </View>
+          </Text>
+        }
+      </View>
+
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5B9053',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+
+  }, logo: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  }, head: {
+    marginTop: 42,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 375,
+    height: 55
+  }, linha: {
+    backgroundColor: '#FFFFFF',
+    height: 0.34,
+    width: 408,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5
+
+  }, mais: {
+    color: '#66B25A',
+    fontWeight: 'bold',
+    width: 82,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }, titulo: {
+    fontWeight: 'bold',
+    marginBottom: 12
+  }, btnCalcular: {
+    backgroundColor: '#C2B1B1',
+    padding: 10,
+    borderRadius: 5,
+    width: 80,
+    height: 38,
+    opacity: 0.6
+  }, conteudoAtençao: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 45,
+    width: 362,
+    height: 140,
+    marginTop: 60,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5
+
+  }
+
+});
